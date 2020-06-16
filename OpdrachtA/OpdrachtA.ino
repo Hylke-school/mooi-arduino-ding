79b627f8770e62db1ef66cba3af92e2b4c406291
unknown <hylke.van.der.zee@student.nhlstenden.com>
2020-06-16 21:46:20 +0200

    added notifications code to opdrachta.ino

--- OpdrachtA/OpdrachtA.ino
+++ OpdrachtA/OpdrachtA.ino
@@ -10,6 +10,7 @@ int buttonPin = 2;
 boolean beenPressed = false;
 
 EthernetServer server(ethPort);              // EthernetServer instance (listening on port <ethPort>)
+EthernetClient client;                       // EthernetClient instance (for sending http request for notifications)
 
 void setup()
 {
@@ -24,13 +25,13 @@ void setup()
     Serial.println("Could not obtain IP-address from DHCP -> do nothing");
     while (true){ }   // no point in carrying on, so do nothing forevermore; check your router
   }
-
   //Start the ethernet server.
   server.begin();
   Serial.print("Listening address: ");
   Serial.print(Ethernet.localIP());
   Serial.print("\t Listening port: ");
   Serial.println(ethPort);
+  sendNotification();
 }
 
 void loop()
@@ -130,6 +131,7 @@ void doorBellPress()
   Serial.println("\nBell has been pressed");
   beenPressed = true;
   server.write("BEL\n");
+  sendNotification();
 }
 
 //Close and lock the package box
@@ -152,4 +154,46 @@ bool checkBoxStatus()
   
   
   return false;
+}
+//Sends http request to custom PHP script, that sends HTTPS request to Firebase, to trigger notification in app
+//  data = JSON code for notification
+//  server = server address, not used
+//  IMPORTANT, DNS does not work with this script, so you have to manually resolve the IP address and fill it in at client.connect!
+void sendNotification(){
+  
+  String data = "{\"to\":\"/topics/notifications\",\"notification\": {\"body\": \"Someone rang your doorbell!\",\"title\" : \"Doorbell\",\"Image\":\"https://cdn1.iconfinder.com/data/icons/hands-pt-2/100/095_-_hand_ring-512.png\"}}";
+  char server[] = "http://mooi-deurbel-ding.000webhostapp.com/api.php";
+  
+  Serial.print("connecting to ");
+  Serial.print(server);
+  Serial.println("...");
+  Serial.print("With data:");
+  Serial.print(data);
+  Serial.println("...");
+
+  if (client.connect("145.14.144.89", 80))  {
+   Serial.println("Connected to the server..");
+   Serial.println(client.remoteIP());
+   client.println("POST /api.php HTTP/1.1");
+   client.println("Authorization: key=AAAAiDM7u40:APA91bGO0pMhKbF_eMvLoP-k4Mxz16pbLn3WP-zsyL_DISf1q1KJmohQIly3zHwnH4zyZkeRqZrC-2yJ42RSgJA4i9x2Y1L5Rtvc-4QsBaAYj0mQOcFWC4J3_cYL951GaoJ-b9reKs5c");
+   client.println("Host: mooi-deurbel-ding.000webhostapp.com");
+   client.println("Content-Type: application/json");
+   client.print("Content-Length: ");
+   client.println(data.length());
+   client.print("\n");
+   client.print(data);
+  } else {
+    Serial.println("connection failed");
+  }
+
+  Serial.println("Data sent...Reading response..");
+  while (client.available())  {
+   char c = client.read();
+   Serial.print(c);
+  }
+
+  Serial.println("Finished!");
+  client.flush();
+  client.stop();
+  
 }
