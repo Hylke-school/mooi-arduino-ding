41651f62f6f90773124b4b5115b77d8f3a19a22a
Guerzoniansus <weezimonkey@gmail.com>
2020-06-26 02:51:25 +0200

    Final version, Amy's code

--- SmartDoorbell/SmartDoorbell.ino
+++ SmartDoorbell/SmartDoorbell.ino
@@ -3,30 +3,38 @@
 #include <Ethernet.h>             // Ethernet library (use Ethernet2.h for new ethernet shield v2)
 
 // Set Ethernet Shield MAC address  (check yours)
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; 
 int ethPort = 11250;
 
-boolean boxLocked = false;
-boolean buttonPressed = false;
+boolean isBoxClosed = false; // Bool that keeps track of the box status (open or closed)
 
-int buttonPin = 2; //Pin where the doorbell is connected
-int trigPin1 = 3; //Pin wehere the trigger point of the first ultrasonic sensor is connected
-int trigPin2 = 4; //Pin wehere the trigger point of the second ultrasonic sensor is connected
-int echoPin1 = 5; //Pin where the echo point of the first ultrasonic sensor is connected
-int echoPin2 = 6; //Pin where the echo point of the second ultrasonic sensor is connected
+#define TRIGGER_PIN_SENSOR1   2     // Pin where the trigger point 1 on the ultrasonic sensor is connected.
+#define ECHO_PIN_SENSOR1      3     // Pin where the echo point 1 of the ultrasonic sensor is connected.
+#define TRIGGER_PIN_SENSOR2   8     // Pin where the trigger point 2 on the ultrasonic sensor is connected.
+#define ECHO_PIN_SENSOR2      9     // Pin where the echo point 2 of the ultrasonic sensor is connected.
+#define SWITCH_PIN            7     // Pin where the switch of the box is connected.
+#define LOCK_PIN              6     // Pin where the mechanical lock of the box is connected.
 
-EthernetServer server(ethPort);              // EthernetServer instance (listening on port <ethPort>)
-EthernetClient client;                       // EthernetClient instance, for sending POST request
+#define BELL_TIMEOUT 7000     // Timeout of 7 seconds for the bell (how long the AnalogRead will read 0 to notice if the bell has been pressed).
+#define UNLOCK_TIMEOUT 10000 // Timout of 10 seconds for the lock, it takes 10 seconds and then the lock closes automatically.
+
+unsigned long dingDongTimeout = millis() + dingDongTimeout;
+unsigned long unlockTimeout = 0;
+
+EthernetServer server(ethPort);     // EthernetServer instance (listening on port <ethPort>)
+EthernetClient client;              // EthernetClient instance, for sending POST request
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  pinMode(buttonPin, INPUT);
-  pinMode(trigPin1, OUTPUT);
-  pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(echoPin2, INPUT);
+  //pinMode(DOORBEL_PIN, INPUT);
+  pinMode(TRIGGER_PIN_SENSOR1, OUTPUT);
+  pinMode(ECHO_PIN_SENSOR1, INPUT);
+  pinMode(TRIGGER_PIN_SENSOR2, OUTPUT);
+  pinMode(ECHO_PIN_SENSOR2, INPUT);
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  pinMode(LOCK_PIN, OUTPUT);
 
   Serial.println("Domotica project, Arduino Domotica Server\n");
 
@@ -34,7 +42,7 @@ void setup()
   if (Ethernet.begin(mac) == 0)
   {
     Serial.println("Could not obtain IP-address from DHCP -> do nothing");
-    while (true){ }   // No point in carrying on, so do nothing forevermore; check your router
+    while (true){ }   // no point in carrying on, so do nothing forevermore; check your router
   }
 
   //Start the ethernet server.
@@ -47,12 +55,12 @@ void setup()
 
 void loop()
 {
-  TriggerNotification(buttPin);
-  
-  // Listen for incomming connection (app)
+  doorBellPressed();
+  unlockTimer();
+  // Listen for incomming connection (app).
   EthernetClient ethernetClient = server.available();
   if (!ethernetClient) {
-    return; // wait for connection
+    return; // Wait for connection.
   }
 
   Serial.println("Application connected");
@@ -67,7 +75,8 @@ void loop()
       executeCommand(inByte);
       inByte = NULL;
     } 
-    TriggerNotification(buttPin);
+    doorBellPressed();
+    unlockTimer();
   }
   
   Serial.println("Application disconnected");
@@ -78,16 +87,16 @@ void loop()
 void executeCommand(char cmd)
 {     
   //*  Command explanation 
-  //*  L:  Lock package box
-  //*      Call the function that will close and lock the package box
-  //*    Returns: nothing
+  //*  L:  Lock package box.
+  //*      Call the function that will close and lock the package box.
+  //*    Returns: nothing.
   //*  U:  Unlock the package box
   //*      Calls the function that will open the package box.
-  //*    Returns: nothing
-  //*  S: Status of package box
-  //*    Returns: CLS if box is closed and locked, returns OPN if box is unlocked
-  //*  P: Status of package, present or not
-  //*    Returns: YES if box contains package, NO if box does not contain package
+  //*    Returns: nothing.
+  //*  S: Status of package box.
+  //*    Returns: CLS if box is closed, returns OPN if box is open.
+  //*  P: Status of package, present or not.
+  //*    Returns: YES if box contains package, NO if box does not contain package.
   
   // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   // !!!!!!!!!!!!!  IMPORTANT  !!!!!!!!!!!!!
@@ -96,12 +105,11 @@ void executeCommand(char cmd)
   // Responses need to be 4 bytes (see below, see Connection class in app) and need to end with with "\n"
   // Syntax to respond: server.write(buf, len). Note "len": you need to specify the length of the buffer (4). Don't forget!
   
-  char buf[4] = {'\0', '\0', '\0', '\0'}; 
+ // char buf[4] = {'\0', '\0', '\0', '\0'}; 
   
   // Command protocol
-  Serial.print("["); Serial.print(cmd); Serial.print("] -> ");
-  switch(cmd)
-  {
+  Serial.print("["); Serial.print(cmd); Serial.println("] -> ");
+  switch (cmd) {
     case 'l':
       closePackageBox();
       break;
@@ -113,55 +121,62 @@ void executeCommand(char cmd)
         server.write("CLS\n", 4);
       else 
         server.write("OPN\n", 4);
-      break;
+        break;
     case 'p':
-      if(HasPackage(trigPin1, echoPin1, 25) || HasPackage(trigPin2, echoPin2, 25)){ //Limit set to 25, change according to the box size. 
+      switchPressed();
+      if (hasPackage(TRIGGER_PIN_SENSOR1, ECHO_PIN_SENSOR1, 25) || hasPackage(TRIGGER_PIN_SENSOR2, ECHO_PIN_SENSOR2, 25)) {
         server.write("YES\n", 4);
       }
       else {
         server.write(" NO\n", 4);
       }
-  }
+    }
 }
 
 //Close and lock the package box
 void closePackageBox()
 {
-  boxLocked = true;
+  digitalWrite(LOCK_PIN, LOW);
 }
 
-//Open the package box, start to check if the distancesensor value has changed
+//Open the package box, start to check if the weightsensor value has changed
 void openPackageBox() 
 {
-  boxLocked = false;
+  digitalWrite(LOCK_PIN, HIGH);
+  unlockTimeout = millis();
 }
 
 //Check the status of the box
 // False = open / unlocked
 // True = Closed / locked
 bool checkBoxStatus() 
-{
-  return boxLocked;
+{  
+  return isBoxClosed;
 }
 
 //Checks if the box contains an object using the ultrasonic sensor
 //  trig = pin where trigger point is connected (use trigPin)
 //  echo = pin where echo point is connected (use echoPin)
 //  limit = maximum distance required to return true (I recommend the length of the box - 5)
-bool HasPackage(int trig, int echo, int limit){
+bool hasPackage(int trig, int echo, int limit){
   float v = 0.0343; // Speed of sound at 20 degrees Celsius in centimeters/microsecond;
   long t; // Time
   int s; // Distance
-
+  
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
   // Sends out a soundwave
   digitalWrite(trig, HIGH);
-  delay(10);
+  delayMicroseconds(50);
   digitalWrite(trig, LOW);
 
-  t = pulseIn(echo, HIGH, 2333); //Returns the time between start and pulse recieved in microseconds
-
+  t = pulseIn(echo, HIGH, 12333); //Returns the time between start and pulse recieved in microseconds
+  
   s = v * (0.5 * t); //Calculates the distance to an object in centimeters
-
+  Serial.println(s);
+  if (s == 0) {
+    return false;
+  }
   if(s < limit){
     return true;
   }
@@ -170,7 +185,6 @@ bool HasPackage(int trig, int echo, int limit){
   }
 
 }
-
 //Sends http request to custom PHP script, that sends HTTPS request to Firebase, to trigger notification in app
 //  data = JSON code for notification
 //  server = server address, not used
@@ -207,16 +221,28 @@ void sendNotification(){
   client.stop();
 }
 
-void TriggerNotification(int button){
-  if(buttonPressed == false){
-    if(digitalRead(button) == 0){
-      sendNotification();
-      buttonPressed = true;
-    }
+void doorBellPressed() {
+  if (millis() - dingDongTimeout > BELL_TIMEOUT) {
+      int val = analogRead(0);
+      if (val > 300) {
+          dingDongTimeout = millis();
+          sendNotification();
+      }
+   }
+}
+
+void unlockTimer() {
+  if (unlockTimeout == 0) return;  // Timer not active
+  if (millis() - unlockTimeout > UNLOCK_TIMEOUT) {
+    unlockTimeout = 0; //millis();
+    closePackageBox();
   }
-  else{
-    if(digitalRead(button) != 0){
-      buttonPressed = false;
-    }
-  }
+}
+
+void switchPressed() {
+  //read the pushbutton value into a variable
+  int sensorValue = digitalRead(SWITCH_PIN);
+
+  isBoxClosed = sensorValue == HIGH;
+
 }
